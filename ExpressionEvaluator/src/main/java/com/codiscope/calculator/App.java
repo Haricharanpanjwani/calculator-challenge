2cd3d4d452b210e901dd3fb105fbcf9e948e2d83
Hari Panjwani <hpanjwani@Haris-MacBook-Pro.local>
2017-06-23 19:06:32 -0400

    Initial push - version 1.0

---
+++ ExpressionEvaluator/src/main/java/com/codiscope/calculator/App.java
@@ -0,0 +1,29 @@
+package com.codiscope.calculator;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class App {
+	
+	private static final Logger LOGGER = LoggerFactory.getLogger(App.class.getName());
+		
+		public static void main(String[] args) {
+			String expression = "div(2,0)";
+
+			/*
+			if(args.length > 0)
+				expression = args[0];
+			else
+				System.out.println("Please enter the argrument");
+			*/
+			ExpressionEvaluator calc = new ExpressionEvaluator();
+			
+			try{
+				LOGGER.info("Performing the operation : " + expression);
+				double result = calc.evaluateExpression(expression);
+				System.out.println(result);				
+			}catch(Exception e){
+				LOGGER.error(String.format("Exception found: %1$s", e.getMessage()), e);
+			}
+		}
+}
