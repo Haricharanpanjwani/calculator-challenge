8bdcf9263ee9542f3712b3d4ffdb77e1fd188c9b
Hari Panjwani <hpanjwani@Haris-MacBook-Pro.local>
2017-06-24 16:32:32 -0400

    testing code build

--- ExpressionEvaluator/src/main/java/com/codiscope/calculator/App.java
+++ ExpressionEvaluator/src/main/java/com/codiscope/calculator/App.java
@@ -2,28 +2,34 @@ package com.codiscope.calculator;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.slf4j.event.Level;
 
 public class App {
-	
-	private static final Logger LOGGER = LoggerFactory.getLogger(App.class.getName());
-		
-		public static void main(String[] args) {
-			String expression = "add(2,mult(5,3))";
 
-			/*
-			if(args.length > 0)
-				expression = args[0];
-			else
-				System.out.println("Please enter the argrument");
-			*/
-			ExpressionEvaluator calc = new ExpressionEvaluator();
-			
-			try{
-				LOGGER.info("Performing the operation : " + expression);
-				double result = calc.evaluateExpression(expression);
-				System.out.println(result);				
-			}catch(Exception e){
-				LOGGER.error(String.format("Exception found: %1$s", e.getMessage()), e);
+	private static final Logger LOGGER = LoggerFactory.getLogger(App.class.getName());
+	
+	public static void main(String[] args) {
+		
+		String expression = null;
+
+	
+		if(args.length > 0) 
+			expression = args[0]; 
+		else
+			System.out.println("Please enter the argrument");
+		
+
+		ExpressionEvaluator calculator = new ExpressionEvaluator();
+
+		try {
+			LOGGER.info("Performing the operation : " + expression);
+			double result = calculator.evaluateExpression(expression);
+			if(result != Integer.MAX_VALUE) {
+				LOGGER.info("Result of the operation : " + result);
+				System.out.println(result);
 			}
+		} catch (Exception e) {
+			LOGGER.error(String.format("Exception found: %1$s", e.getMessage()), e);
 		}
+	}
 }
--- ExpressionEvaluator/src/main/java/com/codiscope/calculator/ExpressionEvaluator.java
+++ ExpressionEvaluator/src/main/java/com/codiscope/calculator/ExpressionEvaluator.java
@@ -1,88 +1,109 @@
 package com.codiscope.calculator;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.Hashtable;
+import java.util.Map;
 import java.util.regex.Pattern;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-
-
 public class ExpressionEvaluator {
 
-	private Pattern pattern = Pattern.compile("\\d+");
-	private Hashtable<Object, Double> operations = new Hashtable<Object, Double>();
 	private static final Logger LOGGER = LoggerFactory.getLogger(ExpressionEvaluator.class.getName());
-	
+
+	private Pattern pattern = Pattern.compile("\\d+");
+	private Map<Object, Double> operations = new HashMap<Object, Double>();
+
 	private double evaluate(Object expList) {
-	    if (expList instanceof String) {     
-	        if (pattern.matcher(expList.toString()).matches()) {
-	            return Integer.parseInt(expList.toString());
-	        }else{
-	        	return operations.get(expList.toString());
-	        }
-	    }
-	    else {
-	    	ArrayList<?> expressionList = (ArrayList<?>)expList;
-	    	String operator = expressionList.get(0).toString();
-	    	Object exp = expressionList.get(1);
-	    	if (operator.equalsIgnoreCase("start")) {
-	            return evaluate(exp);
-	        }else if (operator.equalsIgnoreCase("add")) {
-	        	LOGGER.info("Performing addition");
-	            return evaluate(exp) + evaluate(expressionList.get(2));
-	        }else if (operator.equalsIgnoreCase("mult")) {
-	        	LOGGER.info("Performing multiplication");
-	            return evaluate(exp) * evaluate(expressionList.get(2));
-	        }else if (operator.equalsIgnoreCase("div")) {
-	        	LOGGER.info("Performing division");
-	            return evaluate(exp) / evaluate(expressionList.get(2));
-	        }else if (operator.equalsIgnoreCase("let")) {
-	        	LOGGER.info("Performing let operation");
-	        	operations.put(exp, evaluate(expressionList.get(2)));
-	    	    return evaluate(expressionList.get(3));
-	        }else{
-	        	LOGGER.error("Incorrect operation");
-	        	return 0;
-	        }
-	    }
-	}	 
-	
-	private Object[] parseExp(String expression, String operation, int index) throws Exception {
-	    ArrayList<Object> expList = new ArrayList<Object>();
-	    expList.add(operation);
-	    String temp = "";
-	    try{
-		    while(true) {
-		    	char charPoint = expression.charAt(index);
-		        if(charPoint == '(') {
-		        	Object[] returned = parseExp(expression, temp, index+1);
-		        	index = (Integer) returned[1];
-		        	expList.add(returned[0]);
-		        	temp = "";
-		        	if (operation.equalsIgnoreCase("start")) 
-		        		break;
-		        }else if (charPoint == ')') {
-		        	if (temp.length() > 0) expList.add(temp);
-		        	break;
-		        }else if (charPoint == ',') {
-		            if (temp.length() > 0) expList.add(temp);
-		            temp = "";
-		        }else{
-		            temp += expression.charAt(index);
-		        }
-	        	++index;
-		    }
-	    }catch (Exception e) {
-	    	LOGGER.error(String.format("Exception found: %1$s", e.getMessage()), e);
-	    	e.printStackTrace();
+
+		if (expList instanceof String) {
+			
+			if (pattern.matcher(expList.toString()).matches())
+				return Integer.parseInt(expList.toString());
+			
+			else
+				return operations.get(expList.toString());
+		} 
+		else {
+			
+			ArrayList<?> expressionList = (ArrayList<?>) expList;
+
+			String operator = expressionList.get(0).toString();
+			Object exp = expressionList.get(1);
+
+			if (operator.equalsIgnoreCase("start"))
+				return evaluate(exp);
+
+			else if (operator.equalsIgnoreCase("add")) {
+
+				LOGGER.info("Performing addition");
+				LOGGER.debug("Performing addition in debug mode");
+				return evaluate(exp) + evaluate(expressionList.get(2));
+			} else if (operator.equalsIgnoreCase("mult")) {
+
+				LOGGER.info("Performing multiplication");
+				return evaluate(exp) * evaluate(expressionList.get(2));
+			} else if (operator.equalsIgnoreCase("div")) {
+
+				LOGGER.info("Performing division");
+				return evaluate(exp) / evaluate(expressionList.get(2));
+			} else if (operator.equalsIgnoreCase("let")) {
+
+				LOGGER.info("Performing let operation");
+				operations.put(exp, evaluate(expressionList.get(2)));
+				return evaluate(expressionList.get(3));
+			} else {
+
+				LOGGER.error("Invalid operation");
+				return Integer.MAX_VALUE;
+			}
+		}
+	}
+
+	private Object[] parseExp(String expression, String operation, int index) {
+		
+		ArrayList<Object> expList = new ArrayList<Object>();
+		
+		expList.add(operation);
+		String temp = "";
+		
+		try {
+			while (true) {
+				char charPoint = expression.charAt(index);
+				if (charPoint == '(') {
+					Object[] returned = parseExp(expression, temp, index + 1);
+					index = (Integer) returned[1];
+					expList.add(returned[0]);
+					temp = "";
+					
+					if (operation.equalsIgnoreCase("start"))
+						break;
+					
+				} else if (charPoint == ')') {
+					if (temp.length() > 0)
+						expList.add(temp);
+					break;
+					
+				} else if (charPoint == ',') {
+					if (temp.length() > 0)
+						expList.add(temp);
+					temp = "";
+				} else {
+					temp += expression.charAt(index);
+				}
+				++index;
+			}
+		} catch (Exception e) {
+			LOGGER.error(String.format("Exception found: %1$s", e.getMessage()), e);
+			e.printStackTrace();
 		}
 
-	    return new Object[]{expList, index};  
+		return new Object[] { expList, index };
 	}
-	
-	public double evaluateExpression(String expression) throws Exception{
+
+	public double evaluateExpression(String expression) {
 		LOGGER.info("Operation is started");
 		return evaluate(parseExp(expression.replaceAll(" ", ""), "start", 0)[0]);
 	}
